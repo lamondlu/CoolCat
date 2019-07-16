ce420521a67a083686099079a5c72dbb41de976a
Lamond Lu <lunan@shintechchina.com>
2019-07-16 21:21:16 +0800

    add new core

---
+++ DynamicPlugins.Core/Business/Version.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicPlugins.Core.Business
+{
+    public class Version
+    {
+        public string VersionNumber { get; set; }
+
+        public static bool operator ==(Version left, Version right)
+        {
+            if (left == null || right == null)
+            {
+                return false;
+            }
+
+            return left.VersionNumber.Equals(right.VersionNumber);
+        }
+
+        public static bool operator !=(Version left, Version right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ DynamicPlugins.Core/Contracts/IModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicPlugins.Core.Contracts
+{
+    public interface IModule
+    {
+        string Name { get; }
+
+        Business.Version Version { get; set; }
+    }
+}
