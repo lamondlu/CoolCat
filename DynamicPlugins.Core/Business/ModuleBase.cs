5f7746c2e9811c7d8b7cf7cb13da1b5a19d85235
Lamond Lu <lunan@shintechchina.com>
2019-07-17 05:33:42 +0800

    add reference

--- DynamicPlugins.Core/Business/ModuleBase.cs
+++ DynamicPlugins.Core/Business/ModuleBase.cs
@@ -36,5 +36,9 @@ namespace DynamicPlugins.Core.Business
             get;
             private set;
         }
+
+        public string DllPath { get; set; }
+
+        public string ViewDllPath { get; set; }
     }
 }
