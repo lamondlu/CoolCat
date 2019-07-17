b02ecfa44e7fa06572a8cd8bcee464bb84469fbf
Lamond Lu <lunan@shintechchina.com>
2019-07-17 10:51:38 +0800

    add database project

---
+++ DynamicPlugins.Database/Modules.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Modules]
+(
+	[ModuleId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [UniqueKey] NVARCHAR(50) NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [DisplayName] NVARCHAR(MAX) NULL, 
+    [Version] NVARCHAR(50) NOT NULL, 
+    [DllPath] NVARCHAR(MAX) NULL, 
+    [ViewPath] NVARCHAR(MAX) NULL
+)
