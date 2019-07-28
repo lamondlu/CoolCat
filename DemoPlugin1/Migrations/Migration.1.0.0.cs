409cd84720aa07543141fab82dd0746448fec1c6
Lamond Lu <lunan@shintechchina.com>
2019-07-29 07:05:29 +0800

    add code

---
+++ DemoPlugin1/Migrations/Migration.1.0.0.cs
@@ -0,0 +1,25 @@
+﻿using DynamicPlugins.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoPlugin1.Migrations
+{
+    public class Migration_1_0_0 : IMigration
+    {
+        public string Version
+        {
+            get { return "1.0.0"; }
+        }
+
+        public void Down()
+        {
+
+        }
+
+        public void Up()
+        {
+
+        }
+    }
+}
---
+++ DynamicPlugins.Core/Contracts/IMigration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicPlugins.Core.Contracts
+{
+    public interface IMigration
+    {
+        string Version { get; }
+
+        void Up();
+
+        void Down();
+    }
+}
