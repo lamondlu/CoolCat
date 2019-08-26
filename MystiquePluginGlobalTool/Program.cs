497f11c7b666510218072fca9ff7f067b03efa42
Lamond Lu <lunan@shintechchina.com>
2019-08-26 19:57:05 +0800

    init the global tool

---
+++ MystiquePluginGlobalTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MystiquePluginGlobalTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
