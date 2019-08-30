808696fd9ba801011774ef4fbf8701893bb7ebd5
Lamond Lu <lunan@shintechchina.com>
2019-08-30 15:25:12 +0800

    temp check-in

--- MystiquePluginGlobalTool/Program.cs
+++ MystiquePluginGlobalTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace MystiquePluginGlobalTool
 {
@@ -6,7 +7,20 @@ namespace MystiquePluginGlobalTool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo("dotnet");
+            
+            
+            var startInfo = new ProcessStartInfo("dotnet");
+            startInfo.WorkingDirectory = @"C:\Users\Lamond Lu\source\repos\DynamicPlugins\DemoPlugin1";
+            startInfo.Arguments = "publish -o app";
+            startInfo.UseShellExecute = true;
+           
+            
+
+            process.Start();
+            process.WaitForExit();
+
         }
     }
 }
