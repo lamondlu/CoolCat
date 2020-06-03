273a5995dbd7ef86c5bba5157c1ce2b20dc3aab0
lamondlu <309728709@qq.com>
2020-06-03 11:03:11 -0400

    fix the build issue

--- Mystique.Core.Mvc/Infrastructure/MystiqueStartup.cs
+++ Mystique.Core.Mvc/Infrastructure/MystiqueStartup.cs
@@ -8,6 +8,7 @@ using Mystique.Core.Contracts;
 using Mystique.Core.Helpers;
 using Mystique.Core.Models;
 using Mystique.Core.Repository.MySql;
+using Mystique.Core.Repositories;
 using Mystique.Mvc.Infrastructure;
 using System;
 using System.Collections.Generic;
@@ -28,7 +29,7 @@ namespace Mystique.Core.Mvc.Infrastructure
 
             services.AddSingleton<IMvcModuleSetup, MvcModuleSetup>();
             services.AddScoped<IPluginManager, PluginManager>();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUnitOfWork, Repository.MySql.UnitOfWork>();
             services.AddSingleton<INotificationRegister, NotificationRegister>();
             services.AddSingleton<IActionDescriptorChangeProvider>(MystiqueActionDescriptorChangeProvider.Instance);
             services.AddSingleton<IReferenceContainer, DefaultReferenceContainer>();
