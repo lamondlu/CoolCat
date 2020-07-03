cc5af82e22316b24f6200dd269a53af3f057ce2d
lamondlu <309728709@qq.com>
2020-07-03 15:56:52 +0800

    Update the layout

---
+++ Mystique/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mystique.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
