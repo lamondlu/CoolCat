a554924894b1a2445481c7898d9cac331cb23c85
lamondlu <309728709@qq.com>
2020-07-04 15:54:02 +0800

    add setup module

---
+++ Mystique/wwwroot/js/setup.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    $('.nav-tabs > li a[title]').tooltip();
+
+    //Wizard
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+
+        var target = $(e.target);
+
+        if (target.parent().hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $(".next-step").click(function (e) {
+
+        var active = $('.wizard .nav-tabs li.active');
+        active.next().removeClass('disabled');
+        nextTab(active);
+
+    });
+    $(".prev-step").click(function (e) {
+
+        var active = $('.wizard .nav-tabs li.active');
+        prevTab(active);
+
+    });
+});
+
+function nextTab(elem) {
+    $(elem).next().find('a[data-toggle="tab"]').click();
+}
+function prevTab(elem) {
+    $(elem).prev().find('a[data-toggle="tab"]').click();
+}
+
+
+$('.nav-tabs').on('click', 'li', function () {
+    $('.nav-tabs li.active').removeClass('active');
+    $(this).addClass('active');
+});
