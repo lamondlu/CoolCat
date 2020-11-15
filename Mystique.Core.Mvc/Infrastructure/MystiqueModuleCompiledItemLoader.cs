18c70df1666aa7690c4db4c33107acac0ef7bbce
lamondlu <309728709@qq.com>
2020-11-15 17:18:33 +0800

    Update the code

---
+++ Mystique.Core.Mvc/Infrastructure/MystiqueModuleCompiledItemLoader.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mystique.Core.Mvc.Infrastructure
+{
+    public class MystiqueModuleViewCompiledItemLoader : RazorCompiledItemLoader
+    {
+        public string ModuleName { get; }
+
+        public MystiqueModuleViewCompiledItemLoader(string moduleName)
+        {
+            ModuleName = moduleName;
+        }
+
+        protected override RazorCompiledItem CreateItem(RazorCompiledItemAttribute attribute)
+        {
+            if (attribute == null)
+            {
+                throw new ArgumentNullException(nameof(attribute));
+            }
+
+            return new MystiqueModuleViewCompiledItem(attribute, ModuleName);
+        }
+
+    }
+}
---
+++ Mystique.Core.Mvc/Infrastructure/MystiqueModuleViewCompiledItem.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Razor.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mystique.Core.Mvc.Infrastructure
+{
+    public class MystiqueModuleViewCompiledItem : RazorCompiledItem
+    {
+        public override string Identifier { get; }
+
+        public override string Kind { get; }
+
+        public override IReadOnlyList<object> Metadata { get; }
+
+        public override Type Type { get; }
+
+        public MystiqueModuleViewCompiledItem(RazorCompiledItemAttribute attr, string moduleName)
+        {
+            Type = attr.Type;
+            Kind = attr.Kind;
+            Identifier = "/Modules/" + moduleName + attr.Identifier;
+
+            Metadata = Type.GetCustomAttributes(inherit: true).Select(o =>
+                o is RazorSourceChecksumAttribute rsca
+                    ? new RazorSourceChecksumAttribute(rsca.ChecksumAlgorithm, rsca.Checksum, "/Modules/" + moduleName + rsca.Identifier)
+                    : o).ToList();
+        }
+
+    }
+}
