0f2aed52b1626fd278b55ec463b9c4c7106f680e
lamondlu <309728709@qq.com>
2020-11-18 16:22:01 +0800

    start to add the demo module for books

---
+++ DemoModules/BookInventory/BookInventory/Controllers/BookInventoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookInventory.Controllers
+{
+    public class BookInventoryController : Controller
+    {
+        [HttpGet]
+        public IActionResult Books()
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoModules/BookInventory/BookInventory/ModuleDefiniation.cs
@@ -0,0 +1,14 @@
+﻿using Mystique.Core.Models;
+
+namespace DemoPlugin1
+{
+    public class ModuleDefiniation : Mystique.Core.Models.ModuleDefiniation
+    {
+        public const string MODULE_NAME = "BookInventory";
+
+        public ModuleDefiniation() : base(MODULE_NAME)
+        {
+
+        }
+    }
+}
