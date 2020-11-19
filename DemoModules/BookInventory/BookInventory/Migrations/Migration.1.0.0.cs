c59cfa1d43a6278fbe63176f738d5a32596f3669
lamondlu <309728709@qq.com>
2020-11-19 13:47:24 +0800

    Add migration file

---
+++ DemoModules/BookInventory/BookInventory/Migrations/Migration.1.0.0.cs
@@ -0,0 +1,23 @@
+﻿using Mystique.Core.DomainModel;
+using Mystique.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookInventory.Migrations
+{
+    public class Migration_1_0_0 : BaseMigration
+    {
+        private static readonly Mystique.Core.DomainModel.Version _version = new Mystique.Core.DomainModel.Version("1.0.0");
+
+        public override string UpScripts => @"";
+
+        public override string DownScripts => @"";
+
+        public Migration_1_0_0(DbHelper dbHelper) : base(dbHelper, _version)
+        {
+
+        }
+    }
+}
