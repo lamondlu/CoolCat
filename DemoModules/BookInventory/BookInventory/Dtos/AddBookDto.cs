1bd4f9d2e350e8e2791e611259488094d71cd361
lamondlu <309728709@qq.com>
2020-11-19 23:07:48 +0800

    Add new UI

--- DemoModules/BookInventory/BookInventory/Dtos/AddBookDto.cs
+++ DemoModules/BookInventory/BookInventory/Dtos/AddBookDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,16 @@ namespace BookInventory.Dtos
 {
     public class AddBookDto
     {
+        [Required]
+        public string BookName { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public DateTime DateIssued { get; set; }
+
+        public string Description { get; set; }
+        
     }
 }
