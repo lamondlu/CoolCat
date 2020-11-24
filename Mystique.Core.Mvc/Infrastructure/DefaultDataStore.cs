c78b50f7df02d023f67e045cca79a87700995017
lamondlu <309728709@qq.com>
2020-11-24 22:22:11 +0800

    Update the data store

--- Mystique.Core.Mvc/Infrastructure/DefaultDataStore.cs
+++ Mystique.Core.Mvc/Infrastructure/DefaultDataStore.cs
@@ -9,26 +9,44 @@ namespace Mystique.Core.Mvc.Infrastructure
 {
     public class DefaultDataStore : IDataStore
     {
-       
-
-        public string Query(string moduleName, string queryName, string parameter, Func<string, string> query)
-        {
-            throw new NotImplementedException();
-        }
+        private List<QueryItem> _queryItems = new List<QueryItem>();
 
         public string Query(string moduleName, string queryName, string parameter)
         {
-            throw new NotImplementedException();
+            var query = _queryItems.FirstOrDefault(p => p.ModuleName == moduleName && p.QueryName == queryName);
+
+            if (query != null)
+            {
+                return query.Run(parameter);
+            }
+            else
+            {
+                return string.Empty;
+            }
         }
 
-        public void RegisterQuery(string moduleName, string queryName, string parameter)
+        public void RegisterQuery(string moduleName, string queryName, Func<string, string> query)
         {
-            throw new NotImplementedException();
+            _queryItems.Add(new QueryItem
+            {
+                ModuleName = moduleName,
+                QueryName = queryName,
+                Query = query
+            });
         }
+    }
 
-        public void RegisterQuery(string moduleName, string queryName, string parameter, Func<string, string> query)
+    public class QueryItem
+    {
+        public string ModuleName { get; set; }
+
+        public string QueryName { get; set; }
+
+        public Func<string, string> Query { get; set; }
+
+        public string Run(string parameter)
         {
-            throw new NotImplementedException();
+            return Query(parameter);
         }
     }
 }
--- Mystique.Core/Contracts/IDataStore.cs
+++ Mystique.Core/Contracts/IDataStore.cs
@@ -8,8 +8,8 @@ namespace Mystique.Core.Contracts
 {
     public interface IDataStore
     {
-        void RegisterQuery(string moduleName, string queryName, string parameter, Func<string, string> query);
-
         string Query(string moduleName, string queryName, string parameter);
+
+        void RegisterQuery(string moduleName, string queryName, Func<string, string> query);
     }
 }
