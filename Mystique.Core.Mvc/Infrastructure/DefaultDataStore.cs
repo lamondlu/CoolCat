422cef5a3fdae134e3a972209093d66d0c4e1850
lamondlu <309728709@qq.com>
2020-11-24 20:12:43 +0800

    Add data store

---
+++ Mystique.Core.Mvc/Infrastructure/DefaultDataStore.cs
@@ -0,0 +1,34 @@
+﻿using Mystique.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mystique.Core.Mvc.Infrastructure
+{
+    public class DefaultDataStore : IDataStore
+    {
+       
+
+        public string Query(string moduleName, string queryName, string parameter, Func<string, string> query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Query(string moduleName, string queryName, string parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterQuery(string moduleName, string queryName, string parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterQuery(string moduleName, string queryName, string parameter, Func<string, string> query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
