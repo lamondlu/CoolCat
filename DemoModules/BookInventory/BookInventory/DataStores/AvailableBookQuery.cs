0136405131d140c88414602dd46f709624706a66
lamondlu <309728709@qq.com>
2020-11-25 22:26:51 +0800

    Add query

---
+++ DemoModules/BookInventory/BookInventory/DataStores/AvailableBookQuery.cs
@@ -0,0 +1,28 @@
+﻿using Mystique.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookInventory.DataStores
+{
+    public class AvailableBookQuery : IDataStoreQuery
+    {
+        public string QueryName
+        {
+            get
+            {
+                return "Available_Books";
+            }
+        }
+        public Func<string, string> Query
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+      
+    }
+}
---
+++ Mystique.Core/Contracts/IDataStoreQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mystique.Core.Contracts
+{
+    public interface IDataStoreQuery
+    {
+        string QueryName { get;}
+
+        Func<string, string> Query { get; }
+    }
+}
