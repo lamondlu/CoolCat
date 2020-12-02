176582ec070e726b1d813de0ded337e07f3cfe64
lamondlu <309728709@qq.com>
2020-12-02 21:06:52 +0800

    Add new demo plugin

---
+++ BookLibrary/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using BookLibrary.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Mystique.Core.Contracts;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Controllers
+{
+    [Area(ModuleDefiniation.MODULE_NAME)]
+    public class BookController : Controller
+    {
+        private readonly IDataStore _dataStore;
+
+        public BookController(IDataStore dataStore)
+        {
+            _dataStore = dataStore;
+        }
+
+        public IActionResult AvailableBooks()
+        {
+            var books = JsonConvert.DeserializeObject<List<BookViewModel>>(_dataStore.Query("BookInventory", "Available_Books", string.Empty, source: ModuleDefiniation.MODULE_NAME));
+
+            return View(books);
+        }
+    }
+}
