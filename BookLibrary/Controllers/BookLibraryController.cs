5cf47b0d33e8a6411dcfc17fafba9efd357cb6ea
lamondlu <309728709@qq.com>
2020-12-02 20:24:19 +0800

    Add new demo plugin

---
+++ BookLibrary/Controllers/BookLibraryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Controllers
+{
+    [Area(ModuleDefiniation.MODULE_NAME)]
+    public class BookLibraryController : Controller
+    {
+    }
+}
