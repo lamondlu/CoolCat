f2e6c88daee6f840091e02f2790dbc72f9dbb818
lamondlu <309728709@qq.com>
2020-12-07 19:58:45 +0800

    Add new code

---
+++ DemoModules/BookInventory/BookInventory/BookInEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using Mystique.Core.Contracts;
+using Newtonsoft.Json;
+using System;
+
+namespace BookInventory
+{
+    public class BookInEventHandler : INotificationHandler
+    {
+        public void Handle(string data)
+        {
+            var obj = JsonConvert.DeserializeObject<BookInEvent>(data);
+        }
+    }
+
+    public class BookInEvent
+    {
+        public Guid BookId { get; set; }
+
+        public DateTime InDate { get; set; }
+    }
+
+}
