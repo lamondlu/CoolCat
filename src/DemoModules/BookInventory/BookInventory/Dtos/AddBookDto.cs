c6c7eee7cf5d669fa8bed0096e311fef16cc14ce
lamondlu <309728709@qq.com>
2020-12-17 14:43:21 +0800

    update the UI

--- src/DemoModules/BookInventory/BookInventory/Dtos/AddBookDto.cs
+++ src/DemoModules/BookInventory/BookInventory/Dtos/AddBookDto.cs
@@ -6,15 +6,16 @@ namespace BookInventory.Dtos
     public class AddBookDto
     {
         [Required]
+        [Display(Name = "Book Name")]
         public string BookName { get; set; }
 
         [Required]
         public string ISBN { get; set; }
 
         [Required]
+        [Display(Name = "Date Issued")]
         public DateTime DateIssued { get; set; }
 
         public string Description { get; set; }
-        
     }
 }
