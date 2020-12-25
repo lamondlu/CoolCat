42f8ec96457b5b293c87b0e31a8fcf5e193c9129
lamondlu <309728709@qq.com>
2020-12-25 20:21:51 +0800

    Add new controller

---
+++ src/InternalModules/PluginManagement/Controllers/PluginController.cs
@@ -0,0 +1,81 @@
+﻿using CoolCat.Core.Contracts;
+using CoolCat.Core.DomainModel;
+using CoolCat.Core.Mvc.Extensions;
+using CoolCat.Core.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PluginManagement.Controllers
+{
+    [Area(ModuleDefiniation.MODULE_NAME)]
+    public class PluginController : CoolCatController
+    {
+        private readonly IPluginManager _pluginManager = null;
+        private readonly IReferenceContainer _referenceContainer = null;
+        private readonly IDbHelper _dbHelper = null;
+        private readonly IQueryDocumentation _queryDocumentation = null;
+
+        public PluginController(IPluginManager pluginManager, IReferenceContainer referenceContainer, IDbHelper dbHelper, IQueryDocumentation queryDocumentation, IDataStore dataStore) : base(ModuleDefiniation.MODULE_NAME, dataStore)
+        {
+            _pluginManager = pluginManager;
+            _referenceContainer = referenceContainer;
+            _dbHelper = dbHelper;
+            _queryDocumentation = queryDocumentation;
+        }
+
+
+        public IActionResult Document()
+        {
+            var documents = _queryDocumentation.GetAllDocuments();
+            return View(documents);
+        }
+
+        public IActionResult Assemblies()
+        {
+            System.Collections.Generic.List<CachedReferenceItemKey> items = _referenceContainer.GetAll();
+
+            return View(items);
+        }
+
+        public IActionResult Index()
+        {
+            return View(_pluginManager.GetAllPlugins());
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Upload()
+        {
+            PluginPackage package = new PluginPackage(Request.GetPluginStream(), _dbHelper);
+            _pluginManager.AddPlugins(package);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Enable(Guid id)
+        {
+            _pluginManager.EnablePlugin(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Disable(Guid id)
+        {
+            _pluginManager.DisablePlugin(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(Guid id)
+        {
+            _pluginManager.DeletePlugin(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
